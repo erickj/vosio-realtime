784656b00a8de67398902ca267ab63921b593bf7
Erick Johnson <erick@ejjohnson.org>
2015-03-01 13:28:22 -0500

    added error codes to ProtocolException

--- src/main/java/io/vos/stun/protocol/ProtocolException.java
+++ src/main/java/io/vos/stun/protocol/ProtocolException.java
@@ -1,13 +1,25 @@
 package io.vos.stun.protocol;
 
+import io.vos.stun.message.Messages.ErrorCode;
+
 public final class ProtocolException extends Exception {
 
   public enum ReasonCode  {
-    FIRST_TWO_BITS_NOT_ZERO,
-    INVALID_MAGIC_COOKIE,
-    INVALID_MESSAGE_LENGTH,
-    UNSUPPORTED_METHOD,
-    UNSUPPORTED_CLASS_FOR_METHOD
+    FIRST_TWO_BITS_NOT_ZERO(ErrorCode.ERROR_400),
+    INVALID_MAGIC_COOKIE(ErrorCode.ERROR_400),
+    INVALID_MESSAGE_LENGTH(ErrorCode.ERROR_400),
+    UNSUPPORTED_METHOD(ErrorCode.ERROR_500),
+    UNSUPPORTED_CLASS_FOR_METHOD(ErrorCode.ERROR_500);
+
+    private ErrorCode errorCode;
+
+    ReasonCode(ErrorCode errorCode) {
+      this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+      return errorCode;
+    }
   }
 
   private final ReasonCode code;
