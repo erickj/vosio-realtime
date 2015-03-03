24b3256378577e19fae5366b4554a36511542795
Erick Johnson <erick@ejjohnson.org>
2015-03-03 17:37:41 -0500

    moved ErrorCode enum to a top level enum in io.vos.stun.protocol

--- src/main/java/io/vos/stun/protocol/ProtocolException.java
+++ src/main/java/io/vos/stun/protocol/ProtocolException.java
@@ -1,12 +1,11 @@
 package io.vos.stun.protocol;
 
-import io.vos.stun.message.Messages.ErrorCode;
-
 public final class ProtocolException extends Exception {
 
   public enum ReasonCode  {
     FIRST_TWO_BITS_NOT_ZERO(ErrorCode.ERROR_400),
     INVALID_MESSAGE_LENGTH(ErrorCode.ERROR_400),
+    UNKNOWN_ATTRIBUTE(ErrorCode.ERROR_420),
     UNSUPPORTED_METHOD(ErrorCode.ERROR_500),
     UNSUPPORTED_CLASS_FOR_METHOD(ErrorCode.ERROR_500);
 
