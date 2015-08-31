5c72cfdd65aac121ce029fcb3ef05e50e8491ede
Erick Johnson <erick@ejjohnson.org>
2015-08-31 11:33:57 +0200

    formatting

--- src/main/java/io/vos/stun/demo/Client.java
+++ src/main/java/io/vos/stun/demo/Client.java
@@ -1,8 +1,8 @@
 package io.vos.stun.demo;
 
-import java.net.DatagramSocket;
-import java.net.DatagramPacket;
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 
 public class Client {
 
@@ -20,10 +20,10 @@ public class Client {
     }
 
     try {
-    DatagramSocket socket = new DatagramSocket();
+      DatagramSocket socket = new DatagramSocket();
 
-    byte[] buffer = new byte[1500];
-    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+      byte[] buffer = new byte[1500];
+      DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
     } catch (IOException e) {
         e.printStackTrace();
     }
